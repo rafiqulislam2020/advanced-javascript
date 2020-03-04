860e9185c2064f7ad0f28128c0f0cab986a664ac
Md.Rafiqul Islam <shopon104@gmail.com>
2020-03-05 01:13:46 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+//let call; //variable value not assign thats why it is undefined
+// if function return value is not assign thats why it is undefined
+// two variable is declare  num1 and num2
+            // function add(num1, num2){
+            //     console.log(num1 + num2);
+            // }
+//if parameter is declare but not pass it is undefined
+        // function add(num1, num2){
+        // console.log(num1, num2);
+        // }
+        // const result = add (13);
+//if object property is not assign it is undefined 
+// const result = add (13);
+// console.log(result);
+let fun = undefined;
+console.log(fun);
