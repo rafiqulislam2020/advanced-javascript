3bfa3c26dba1458fc527467474b433763b2cca64
Md.Rafiqul Islam <shopon104@gmail.com>
2020-03-05 00:32:11 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// all value is false
+//false
+//0
+//""
+//undefined
+//null
+//NaN  -- nana  ----> not a number
+
+//all are value is true
+// '0', '' []
+// if else condition
+const age = 4;
+if (age>0)
+console.log("condition is true");
+else{
+    console.log("condition is false");
+}
+
